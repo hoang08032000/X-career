b18f6ea39f8411944154902a0c73b4d74476c908
hoangtoviet <56767074+hoang08032000@users.noreply.github.com>
2021-04-06 21:06:18 +0700

    Add files via upload

---
+++ x-carrer/main.js
@@ -0,0 +1,25 @@
+function validateTime(time){
+    a = time.split(":");
+    if(parseInt(a[0]) >= 0 && parseInt(a[0]) <= 23 && parseInt(a[1]) >=0 && parseInt(a[1]) <= 59){
+        return true;
+    }
+    else{
+        return false
+    }
+    
+}
+
+
+function missingNumber(arr){
+    let a = (i) => {return i <=arr.length}
+    let b = (i) => {return i >= 0}
+    if(arr.every(a) && arr.every(b) && arr.length>0 && arr.length<1000){
+        for(let i =0; i<=arr.length; i++){
+            if(!arr.includes(i)){
+                console.log(i)
+            }
+
+        }
+    }
+}
+missingNumber([1,0,3])
